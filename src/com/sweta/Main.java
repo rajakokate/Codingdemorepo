9e98a2c1b3365dd3d9c8878d6543a3b4ec36e5f9
Sweta <88280052+Sweta2501@users.noreply.github.com>
2022-11-02 16:26:11 +0530

    Add files via upload

--- src/com/sweta/Main.java
+++ src/com/sweta/Main.java
@@ -1,41 +1,98 @@
 package com.sweta;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-            Syntax of if statements:
-            if (boolean expression T or F) {
-                // body
-            } else {
-                // do this
-            }
-        */
+        Scanner in = new Scanner(System.in);
+//    String fruit = in.next();
 
-        int salary = 25400;
-//        if (salary > 10000) {
-//            salary = salary + 2000;
-//        } else {
-//            salary = salary + 1000;
-//        }
+  /*  String a ="Sweta";
+    String b="Sweta";
+    System.out.println( a==b );
+    */
+ /*   if(fruit.equals("mango")){
+        System.out.println("King of fruit");
+    }
+    if(fruit.equals("apple")){
+        System.out.println("A sweet Red fruit");
+    }
 
-        // multiple if-else
+  */
+        // Q: Fruit
+  /*      switch(fruit){
+            case "Mango":
+                System.out.println("King of the fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Smallest fruit");
+                break;
+            default:
+                System.out.println("PLease enter a valid fruit");
 
-//        if (salary > 10000) {
-//            salary += 2000; // salary = salary + 2000
-//        } else if (salary > 20000) {
-//            salary += 3000;
-//        } else {
-//            salary += 1000;
-//        }
-//
-//        System.out.println(salary);
-
-        int a = 10;
-        int b = 40;
-
-        if (a != 35) {
-            System.out.println("Hello World");
         }
+
+   */
+        // If break is not given it will check next line
+
+        /*
+        switch (fruit){
+        case "Mango" ->System.out.println("King of the fruits");
+        case "Apple" ->System.out.println("A sweet red fruit");
+        case "Orange" -> System.out.println("Round fruit");
+        case "Grapes"->System.out.println("Smallest fruit");
+        default ->System.out.println("PLease enter a valid fruit");
+        } */
+        int day = in.nextInt();
+ /*      switch(day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+        }
+*/
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("invalid");
+        }
+
+  /*
+        switch (day) {
+            case 1,2,3,4,5 ->  System.out.println("Weekday");
+            case 6,7 ->  System.out.println("Weekend");
+            }
+   */
+
     }
 }
