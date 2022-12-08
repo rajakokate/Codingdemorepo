caeb34f6e1a4c37c927de8f7d6482977f7e7b460
Sweta <88280052+Sweta2501@users.noreply.github.com>
2022-12-08 22:15:25 +0530

    Add files via upload

---
+++ BubbleSort/SetMismatch.java
@@ -0,0 +1,39 @@
+package com.sweta;
+
+public class SetMismatch {
+    public int[] findErrorNums(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr.length){
+                swap(arr, i, correct);
+            } else{
+                i++;
+            }
+        }
+        /*
+
+        while(i < arr.length){
+            if(arr[i] < arr.length && arr[i] != arr[i]){
+                swap(arr, i, arr[i]);
+            } else{
+                i++;
+            }
+        }
+         */
+
+        //search for first missing number
+        for(int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return new int[] {arr[index], index+1};
+            }
+        }
+        return new int[] {-1, -1};
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
